aeskildsen/md2tex

commit e4437738dc178dff7ddc3c8afa73ad90572d3c56

fix bug in inline code reinsertion in case of code containing closing brackets

utils/converters.py
@@ -2,7 +2,7 @@ import re
 from itertools import chain
 
 from .minted import languages
-from .helpers import process_list_indentation
+from .helpers import process_list_indentation, get_matching_brackets
 from .errors_warnings import Warnings
 
 # ---------------------------------------------------------------
@@ -311,10 +311,10 @@ class MDCode:
         :return: the updated string representation
         """
         
-        matches = re.finditer(r"`([^`^\n]+)`", string)
+        matches = re.finditer(r"`(.+?)`", string)
         for m in matches:
             code = m[0]
-            inline_snippet = r"\mintinline{" + minted_language + r"}{" + m[1] + r"}"
+            inline_snippet = r"\mintinline{" + minted_language + "}{" + m[1] + "}"
             string = string.replace(code, inline_snippet)
         
         return string
@@ -511,16 +511,29 @@ class MDCleaner:
         # for that, store all code blocks in a dict, replace them in `string`
         # with a special token. this token uses `+` because they aren't LaTeX
         # special characters
-        block_codematch = re.finditer(r"\\begin\{listing}(.|\n)*?\\end\{listing}", string, flags=re.M)
-        inline_codematch = re.finditer(r"\\mintinline\{.*?\}\{.*?\}", string, flags=re.M)
-
         n = 0
         codedict = {}
-        for match in chain(block_codematch, inline_codematch):
+        
+        block_codematch = re.finditer(r"\\begin\{listing}(.|\n)*?\\end\{listing}", string, flags=re.M)
+        for match in block_codematch:
             code = match[0]  # extract text
             string = string.replace(code, f"@@CODETOKEN{n}@@")
             codedict[f"@@CODETOKEN{n}@@"] = code
             n += 1
+        
+        # \mintinline envs are trickier to match, because we might match the
+        # closing } too soon or too late (greedy vs. lazy quantifiers)
+        # so we use a greedy quantifiyer and enlist a helper function
+        # to properly match the correct number of {}
+        inline_codematch = re.finditer(r"(\\mintinline\{.*?\})(\{.*\})", string, flags=re.M)
+        for match in inline_codematch:
+            mintinline_prefix = match[1]
+            code_arg = match[2]
+            code_arg = get_matching_brackets(code_arg)
+            codedict[f"@@CODETOKEN{n}@@"] = mintinline_prefix + code_arg
+            string = string.replace(match[0], f"@@CODETOKEN{n}@@")
+            n+=1
+
 
         string = string.replace("{==", "") # begin highlight (removed)
         string = string.replace("==}", "") # end highlight (removed)

utils/helpers.py
@@ -75,3 +75,31 @@ def process_list_indentation(lstext):
             prev = li[1]
 
     return lsitems
+
+
+def get_matching_brackets(code: str):
+    r"""
+    Finds and returns the substring of the input code contained by an 
+    outer pair of matching opening and closing curly brackets `{...}`, assuming that any inner pairs of brackets which are opened are closed again.
+    
+    Escaped brackets `\{` and `\}` are ignored.
+    
+    :param code: The input string containing code with curly brackets.
+    :return: The substring from the start of the input code up to and including
+             the matching closing bracket.
+    """
+    pos = 0
+    numOpened = 0
+    hasOpened = False
+    for c in code:
+        if c == '{' and code[pos-1] != "\\":
+            numOpened += 1
+            hasOpened = True
+        elif c == '}' and code[pos-1] != "\\":
+            numOpened -= 1
+            if numOpened == 0 and hasOpened:
+                # we found the closing bracket
+                pos += 1
+                break
+        pos += 1
+    return code[:pos]

commit fa8bc4f014feb3aa1d1f01128415d218bc1de592

fix inline code escaping bug introduced in commit e443773

utils/converters.py
@@ -523,17 +523,24 @@ class MDCleaner:
             n += 1
         
         # \mintinline envs are trickier to match, because we might match the
-        # closing } too soon or too late (greedy vs. lazy quantifiers)
-        # so we use a greedy quantifiyer and enlist a helper function
-        # to properly match the correct number of {}
-        inline_codematch = re.finditer(r"(\\mintinline\{.*?\})(\{.*\})", string, flags=re.M)
-        for match in inline_codematch:
-            mintinline_prefix = match[1]
-            code_arg = match[2]
-            code_arg = get_matching_brackets(code_arg)
-            codedict[f"@@CODETOKEN{n}@@"] = mintinline_prefix + code_arg
-            string = string.replace(match[0], f"@@CODETOKEN{n}@@")
-            n+=1
+        # closing } too soon or too late since it may be part of the code
+        # string. so we matches whole lines containing one ore more \mintinline
+        # commands, then we split those lines by the `\mintline` token.
+        # from there we use a helper function to extract the "outer" open/close
+        # brackets pair in that string, starting from its beginning.
+        for line in re.finditer(r"^.*\\mintinline.*$", string, re.M):
+            segments = re.split(r"\\mintinline", line[0])
+            if len(segments) > 1:
+                for segment in segments[1:]:
+                    print(segment)
+                    mintinline_args = re.finditer(r"(\{.*?\})(\{.*\})", segment, flags=re.M)
+                    for match in mintinline_args:
+                        arg1 = match[1]
+                        arg2 = get_matching_brackets(match[2])
+                        mintinline_command = "\\mintinline" + arg1 + arg2
+                        codedict[f"@@CODETOKEN{n}@@"] = mintinline_command
+                        string = string.replace(mintinline_command, f"@@CODETOKEN{n}@@")
+                        n+=1
 
 
         string = string.replace("{==", "") # begin highlight (removed)